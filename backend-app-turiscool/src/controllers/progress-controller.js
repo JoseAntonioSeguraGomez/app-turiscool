347a56012e2efc3af799bd2c0aa5a4b966cb1097
JoseAntonioSeguraGomez <115014384+JoseAntonioSegura@users.noreply.github.com>
2024-06-28 17:22:34 +0200

    progress

--- backend-app-turiscool/src/controllers/progress-controller.js
+++ backend-app-turiscool/src/controllers/progress-controller.js
@@ -1,4 +1,4 @@
-const url = 'https://academy.turiscool.com/admin/api/v2/progress';
+const url = 'https://academy.turiscool.com/admin/api/v2/users';
 const token = "Bearer 17xa7adwlycG4qrbRatBdCHW41xtl9jNyaBq4d45";
 const id = "62b182eea31d8d9863079f42";
 
@@ -10,5 +10,65 @@ const requestOptions = {
         "Lw-Client": id,
     },
 };
+
+const totalPagesProgress = async (userID) => {
+    console.log(userID)
+    try {
+        const response = await fetch(
+            `${url}/${userID}/progress`,
+            requestOptions
+        );
+        if (!response.ok) {
+            throw new Error("Failed to fetch progress");
+        }
+        const data = await response.json();
+        console.log(response)
+        return data.meta.totalPages;
+    } catch (error) {
+        ("Error:", error);
+    }
+}
+
+export const obtainProgressById = async (req, res) => {
+    const userID = req.params.id;
+
+    const totalPages = await totalPagesProgress(userID);
+    const progressList = [];
+    try {
+        console.log(totalPages)
+
+        for (let i = 0; i <= totalPages; i++) {
+            const response = await fetch(
+                `${url}/${userID}/progress?page=${i}&itemsPerPage=20`,
+                requestOptions
+            );
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch progress");
+            }
+            const data = await response.json();
+            data.data.forEach(progress => {
+                progressList.push(progress);
+            });
+        }
+
+        if (!progressList) {
+            throw new HttpStatusError(404, 'No se encontró ningún progreso');
+        }
+        
+        // Eliminar progresos duplicados
+        const uniqueProgressList = await progressList.filter((progress, index, self) =>
+            index === self.findIndex((t) => (
+                t.id === progress.id
+            ))
+        )
+
+        res.json(uniqueProgressList);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+}
+
 
 
--- backend-app-turiscool/src/routes/index.js
+++ backend-app-turiscool/src/routes/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import course from './course-router.js';
 import user from './user-router.js';
-//import progress from './progress-router.js';
+import progress from './progress-router.js';
 
 const router = express.Router();
 
-router.use('/course', course);
-router.use('/user', user);
-//router.use('/progres', progress);
+router.use('/courses', course);
+router.use('/users', user);
+router.use('/progress', progress);
 
 export default router;
